bea2a7c0727ceae3974269a30b3956d94645835a
SALLA ABHINAYA <141430666+SALLA-ABHINAYA@users.noreply.github.com>
2025-05-13 11:07:06 +0530

    Update variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -3,45 +3,35 @@
 variable "project_id" {
   description = "The GCP project ID to deploy resources into"
   type        = string
+  # No default, should be provided in terraform.tfvars or as an environment variable
 }
 
 variable "region" {
   description = "The GCP region for the resources (e.g., us-central1)"
   type        = string
-  default     = "us-central1" # Example default
+  default     = "us-central1" # Sensible default, can be overridden
 }
 
-# Zone variable is not strictly needed for regional cluster config,
-# but keep it if you need to specify zone for other resources or provider.
-# variable "zone" {
-#   description = "The GCP zone for zonal resources if needed (e.g., us-central1-c)"
-#   type        = string
-#   default     = "us-central1-c" # Example default
-# }
-
-
-
-
 variable "gke_cluster_name" {
-  description = "The name of the GKE cluster"
+  description = "The name of the GKE cluster (existing or to be created if not using data source)"
   type        = string
-  default     = "irmai-gke-cluster"
+  # No default, should be provided in terraform.tfvars
 }
 
 variable "artifact_repo_name" {
-  description = "The name for the Artifact Registry repository"
+  description = "The name for the Artifact Registry repository (existing or to be created if not using data source)"
   type        = string
-  default     = "irmai-docker-repo"
+  # No default, should be provided in terraform.tfvars
 }
 
 variable "node_count" {
   description = "The number of nodes in the GKE node pool"
   type        = number
-  default     = 2
+  default     = 1 # Default node count, can be overridden
 }
 
 variable "node_machine_type" {
   description = "The machine type for the GKE nodes"
   type        = string
-  default     = "n1-standard-8" # Keep e2-medium or change if needed
+  default     = "n1-standard-8" # Default machine type, ensure this meets your needs and budget
 }
