0e2d2f10a1cfa7d5daca25ae475277aa92d429e2
SALLA ABHINAYA <141430666+SALLA-ABHINAYA@users.noreply.github.com>
2025-05-13 11:06:33 +0530

    Update outputs.tf

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,20 +1,19 @@
 # --- outputs.tf ---
 
 output "gke_cluster_name" {
-  description = "The name of the GKE cluster"
-  value       = google_container_cluster.primary.name
+  description = "The name of the GKE cluster (fetched as data)"
+  value       = data.google_container_cluster.primary.name
 }
 
 output "gke_node_pool_name" {
-  description = "The name of the primary node pool"
+  description = "The name of the primary node pool. (Only valid if node pool resource is managed)"
+  # If you removed the google_container_node_pool.primary_nodes resource, remove this output.
   value       = google_container_node_pool.primary_nodes.name
 }
 
-# Updated output to construct the Artifact Registry URL manually
 output "artifact_registry_url" {
-  description = "The URL of the Artifact Registry Docker repository"
-  # The URL format is [LOCATION]-docker.pkg.dev/[PROJECT_ID]/[REPOSITORY_ID]
-  value       = "${var.region}-docker.pkg.dev/${var.project_id}/${google_artifact_registry_repository.docker_repo.repository_id}"
+  description = "The URL of the Artifact Registry Docker repository (fetched as data)"
+  value       = "${data.google_artifact_registry_repository.docker_repo.location}-docker.pkg.dev/${var.project_id}/${data.google_artifact_registry_repository.docker_repo.repository_id}"
 }
 
 output "kubernetes_namespace" {
@@ -22,9 +21,9 @@ output "kubernetes_namespace" {
   value       = kubernetes_namespace.irmaiauth_demo.metadata[0].name
 }
 
-# This output for the LoadBalancer IP will be known only after apply
 output "irmai_service_external_ip" {
-  description = "The external IP address of the Irmai service LoadBalancer"
-  # Use splat expression [*] in case ingress list is empty during plan
-  value       = flatten(kubernetes_service.irmai_service.status[*].load_balancer[*].ingress[*].ip)[0]
+  description = "The external IP address of the irmaiauth-demo-module-service LoadBalancer. May take a few minutes to become available."
+  # Uses try() to prevent errors if the IP is not immediately available during 'terraform plan' or early 'apply'.
+  # The value will be "pending" until the LoadBalancer is fully provisioned and has an IP.
+  value       = try(kubernetes_service.irmaiauth_demo_service.status[0].load_balancer[0].ingress[0].ip, "pending")
 }
